124afc317552053513044ce4fc0e6564a537ccfb
VronzZ <azzuhri2003@gmail.com>
2022-09-22 10:56:46 +0700

    Created Blog App

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
+import imp
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def showBlog(request):
+    context = {}
+    return render(request, 'index.html', context)
